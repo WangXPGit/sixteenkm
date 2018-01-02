197bb3651ea0c063a7bae3ac7595eb6fd038de2e
WangXP <wangxp0711@hotmail.com>
2018-01-02 14:13:01 +0800

    日期工具类中增加月份获取

--- api-framework/src/main/java/com/sixteenkm/api/util/DateUtil.java
+++ api-framework/src/main/java/com/sixteenkm/api/util/DateUtil.java
@@ -11,19 +11,37 @@ import java.util.Date;
  * 日期工具类
  * @project api-framework
  * @author WangXP
- * @since 1.0.0
+ * @since 1.0.1
  * @date 2017-11-30
  */
 public final class DateUtil {
 
 	private static SimpleDateFormat dateTimeFormat = new SimpleDateFormat(
 			"yyyy-MM-dd HH:mm:ss");
+	private static SimpleDateFormat yyyy_dtf = new SimpleDateFormat(
+			"yyyy");
+	private static SimpleDateFormat MM_dtf = new SimpleDateFormat(
+			"MM");
 
 	/**
 	 * 获取当前日期与时间, yyyy-MM-dd HH:mm:ss
 	 */
 	public static String getCurrentDateTime() {
 		return dateTimeFormat.format(new Date());
+	}
+	
+	/**
+	 * 获取当前年份, yyyy
+	 */
+	public static String getCurrentYear() {
+		return yyyy_dtf.format(new Date());
+	}
+	
+	/**
+	 * 获取当前月份, MM
+	 */
+	public static String getCurrentMonth() {
+		return MM_dtf.format(new Date());
 	}
 
 	private DateUtil() {
